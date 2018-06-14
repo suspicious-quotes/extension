ff07154b6285b85f82301ba056d8337c97549f31
veggiedefender <jessejesse123@gmail.com>
2018-06-14 18:30:57 -0400

    add basic slider functionality

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,17 @@
+const slider = document.getElementById('slider');
+const percent = document.getElementById('percent');
+
 (async function() {
   const settings = await loadSettings();
-  console.log(settings);
+  slider.value = settings.intensity * 100;
+  slider.dispatchEvent(new Event('input'));
 })();
+
+slider.addEventListener('input', function() {
+  percent.innerText = slider.value;
+});
+
+slider.addEventListener('change', function() {
+  const intensity = slider.value / 100;
+  saveSettings({ intensity });
+});
