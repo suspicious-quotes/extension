e44017bd62e24fe65dcae854546e030911a52141
veggiedefender <jessejesse123@gmail.com>
2018-06-14 12:21:19 -0400

    recursively find good dom text nodes

--- suspicious-quotes.js
+++ suspicious-quotes.js
@@ -1,1 +1,47 @@
-console.log('hello, world');
+const WHITELIST = new Set([
+  'BODY',
+  'H1', 'H2', 'H3', 'H4', 'H5', 'H6',
+  'UL', 'OL', 'LI',
+  'TABLE', 'TR', 'TH', 'TD',
+  'STRONG', 'EM', 'B', 'I',
+  'FORM', 'INPUT', 'LABEL',
+  'DIV',
+  'SPAN',
+  'P',
+  'A',
+  'BUTTON',
+  'ARTICLE',
+  'ASIDE',
+  'CITE',
+  'DETAILS',
+  'FIGCAPTION',
+  'FOOTER',
+  'HEADER',
+  'MAIN',
+  'MARK',
+  'NAV',
+  'SECTION',
+  'SUMMARY',
+  'TIME',
+]);
+
+function injectQuotes(node) {
+  console.log(node.nodeValue);
+}
+
+function traverseNodes(node) {
+  if (node.nodeType === Node.TEXT_NODE) {
+    if (node.nodeValue && node.nodeValue.trim().length > 1) {
+      injectQuotes(node);
+    }
+  } else {
+    if (node.tagName && !WHITELIST.has(node.tagName)) {
+      return;
+    }
+    for (let i = 0; i < node.childNodes.length; i++) {
+      traverseNodes(node.childNodes[i]);
+    }
+  }
+}
+
+traverseNodes(document.body);
